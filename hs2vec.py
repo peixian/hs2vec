a0397fc6061372d491ebda6d6c9cadf121c43ceb
Peixian Wang <peixian@fastmail.com>
2016-07-28 12:32:40 -0500

    add basic trained model + gitignore

--- hs2vec.py
+++ hs2vec.py
@@ -12,3 +12,24 @@ from operator import itemgetter
 from itertools import combinations
 from unidecode import unidecode
 from multiprocessing import process, managers
+import requests
+import json
+import re
+
+
+
+class card_text(object):
+    def __init__(self, json_name):
+        self.json_name = json_name
+
+    def __iter__(self):
+        card_list = json.load(open(self.json_name))
+        for card in card_list:
+            if 'text' in card:
+                yield self.clean_words(card['text'])
+
+    def clean_words(self, text):
+        text = re.sub(r'(\<.\>)|(\<\/*.\>)', '', text)
+        text = re.sub(r'[^(\/)a-zA-z0-9 ]', '', text)
+        words = text.split()
+        return [word.lower() for word in words if word not in corpus.stopwords.words('english')]
